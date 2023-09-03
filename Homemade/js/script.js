0c7723ebc8d8e29b165be7fae3449a397833aeb5
Dipan46 <57986902+Dipan46@users.noreply.github.com>
2023-09-03 17:40:32 +0530

    Adding Food page

--- Homemade/js/script.js
+++ Homemade/js/script.js
@@ -1,23 +1,3 @@
-
-const menuItems = [
-    { name: "Pizza", price: "$10.99" },
-    { name: "Burger", price: "$7.99" },
-    { name: "Pasta", price: "$9.49" },
-];
-
-// Function to generate menu items dynamically
-function generateMenuItems() {
-    const menuSection = document.querySelector(".menu");
-    menuItems.forEach((item) => {
-        const menuItem = document.createElement("div");
-        menuItem.classList.add("menu-item");
-        menuItem.innerHTML = `
-      <h3>${item.name}</h3>
-      <p>${item.price}</p>
-    `;
-        menuSection.appendChild(menuItem);
-    });
+function openNewPage() {
+    window.location.href = "../../../Foods/foods.html";
 }
-
-// Call the function to generate menu items
-generateMenuItems();
